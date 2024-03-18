3431de894474eaeff1b346ac3a7433c3d5193253
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2024-03-18 15:45:11 -0500

    Complete Lessons 6-8
    Very easy

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+  let total = 0;
+	for(number of numberArray){
+    total += number;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+  let total = 1;
+	for(number of numberArray){
+    total *= number;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	switch(number){
+    case 0:
+    case 1:
+      return 1;
+    default:
+      let factorialResult = 1;
+      for(let i=number;i>1;i--) {
+        factorialResult *= i;
+      }
+      return factorialResult;
+  }
 };
 
 // Do not edit below this line
