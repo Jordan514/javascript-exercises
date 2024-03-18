458cbec7dafcde4947ea7ce821a908e0ea09adee
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2024-03-18 12:27:29 -0500

    Complete lessons 1-3 and first test for 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, valuesToRemove) {
+  let newArray = [];
+  for (const item of array){
+    if(item == valuesToRemove) {
+      continue;
+    } else {
+      newArray.push(item);
+    }
+  }
+  array = newArray;
+  return array;
 };
 
 // Do not edit below this line
