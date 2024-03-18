43855583094ab27ef4ca3358fde48f4c8b73fb0f
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2024-03-18 13:23:33 -0500

    Complete lesson 4
    Wow this was a difficult problem for me
    
    My main issue was that I kept checking and adding the entire set of values available
    I also created some infinite loops when I didnt clear the newArray varaible
    array = newArray;
    newArray = [];

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,42 @@
-const removeFromArray = function(array, valuesToRemove) {
+const removeFromArray = function(array, ...valuesToRemove) {
+  // let newArray = [];
+  // for (const item of array){
+
+  //   for(const valueToRemove of valuesToRemove) {
+  //     if(item == valueToRemove) {
+  //       continue;
+  //     } else {
+  //       newArray.push(item);
+  //     }
+  //   }
+
+  // }
+  // return newArray;
+
+  // let newArray = [];
+  // for (const item of array){
+  //   for(const valueToRemove of valuesToRemove) {
+  //     if(item != valueToRemove) {
+  //       newArray.push(item);
+  //     } else {
+  //       continue;
+  //     }
+  //   }
+  // }
+  // return newArray;
+
   let newArray = [];
-  for (const item of array){
-    if(item == valuesToRemove) {
-      continue;
-    } else {
-      newArray.push(item);
+  for (const valueToRemove of valuesToRemove){
+    for(const item of array) {
+      if(item !== valueToRemove) {
+        newArray.push(item);
+      } else {
+        continue;
+      }
     }
+    array = newArray;
+    newArray = [];
   }
-  array = newArray;
   return array;
 };
 
